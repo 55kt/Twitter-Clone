47b7312d42b820e2f92f3041441018b68101d766
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-02-17 19:07:47 +0200

    Initial Commit

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by Vlad on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
