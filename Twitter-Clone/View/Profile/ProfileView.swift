bece1072cfc53ccbb56110ec1f04fedcf7a400c3
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-02-17 19:23:50 +0200

    Test commit for structure

---
+++ Twitter-Clone/View/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Twitter-Clone
+//
+//  Created by Vlad on 17/2/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }// Body
+}// View
+
+#Preview {
+    ProfileView()
+}
