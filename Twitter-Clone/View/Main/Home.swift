7cfddc9d7ccd3cebb409ee5aa41468ccfa54db37
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-02-17 19:17:26 +0200

    Project Structure

---
+++ Twitter-Clone/View/Main/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Twitter-Clone
+//
+//  Created by Vlad on 17/2/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }// Body
+}// View
+
+#Preview {
+    Home()
+}
